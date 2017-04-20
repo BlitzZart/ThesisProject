ceae3ea13c13e5c90dba8ad7b45f07f5beaff8b7
Daniel Rammer <dnl.rammer@gmail.com>
2017-04-20 17:47:23 +0200

    added steamVR; and did a shitload of fun with new vive trackers

---
+++ Assets/fun/Fun_NetworkManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Fun_NetworkManager : NetworkManager {
+
+    public bool isClient = false;
+
+	void Start () {
+		if (isClient) {
+            StartClient();
+        } else {
+            StartServer();
+        }
+	}
+	
+	void Update () {
+		
+	}
+}
